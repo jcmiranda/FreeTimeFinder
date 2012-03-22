3eede752b1c07a32a4d016f6efeb478cb3ececb5
Jeanette Miranda <jeanette_miranda@brown.edu>
2012-03-22 01:46:37 -0400

    When2Meet testing

---
+++ src/calendar_importers_test/When2MeetTest.java
@@ -0,0 +1,13 @@
+package calendar_importers_test;
+import java.io.IOException;
+
+import calendar_importers.*;
+
+public class When2MeetTest {
+	public static void main(String[] args) throws IOException {
+		String url = "http://www.when2meet.com/?353066-BlwWl";
+		When2MeetImporter wtmi = new When2MeetImporter(url);
+		
+	}
+	
+}
