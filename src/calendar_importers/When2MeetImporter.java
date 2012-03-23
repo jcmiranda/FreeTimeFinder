7712d0eeaebcfd8554d8fa8e202e9e4c478c4b6d
Kelly Buckley <kellybuckley@Kelly-Buckleys-MacBook-Pro.local>
2012-03-22 22:33:46 -0400

    changed imports to be specific not .* in when2meet importer

--- src/calendar_importers/When2MeetImporter.java
+++ src/calendar_importers/When2MeetImporter.java
@@ -1,10 +1,12 @@
 package calendar_importers;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.io.*;
 
 import calendar.CalendarGroup;
 
