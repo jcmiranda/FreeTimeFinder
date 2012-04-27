7c2eb7c5cb193741563281ce015a233545865d87
Roie Levin <eior93@gmail.com>
2012-04-27 00:58:38 -0400

    Aligned days with daylabels

---
+++ src/gui/Day.java
@@ -0,0 +1,106 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.Box;
+import javax.swing.GroupLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import org.joda.time.DateTime;
+
+public class Day extends JPanel{
+
+	private DayPanel _day;
+	private ClickableDayPanel _clickableDay;
+	private DateTime _today;
+	private JLabel _label;
+	private JPanel _labelPanel;
+	
+	private JPanel _hourOfDayLabels;
+	
+	public Day(ClickableDayPanel clickableDay, DayPanel day, DateTime today){
+		super();
+		_day = day;
+		_clickableDay = clickableDay;
+		
+		_label=new JLabel(today.dayOfWeek().getAsShortText());
+		
+		GridBagConstraints c = new GridBagConstraints();
+
+		setLayout(new GridBagLayout());
+		this.setBackground(GuiConstants.LINE_COLOR);
+
+		_labelPanel = new JPanel();
+		_labelPanel.setBackground(GuiConstants.LABEL_COLOR);
+		_labelPanel.add(_label, JPanel.CENTER_ALIGNMENT);
+		
+		JPanel dayGroup = new JPanel();
+		dayGroup.setLayout(new GridLayout(1,2,GuiConstants.LINE_SPACING,0));
+		dayGroup.add(clickableDay);
+		dayGroup.add(day);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.0;
+		c.weightx = 1.0;	
+		c.gridx = 0;
+		c.gridy = 0;		
+		add(_labelPanel, c);
+
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		c.gridx = 0;
+		c.gridy = 1;
+		add(dayGroup, c);
+
+	}
+		
+	public int getLabelHeight(){
+		return _labelPanel.getPreferredSize().height;
+	}
+	
+	public DayPanel getDay(){
+		return _day;
+	}
+
+	public DayPanel getClickableDay(){
+		return _clickableDay;
+	}
+
+	public void setStartHour(int start){
+		_day.setStartHour(start);
+		_clickableDay.setStartHour(start);	
+	}
+
+	public void setNumHours(int num){
+		_day.setNumHours(num);
+		_clickableDay.setNumHours(num);	
+	}
+
+	public void setActive(Boolean active){
+		_day.setActive(active);
+		_clickableDay.setActive(active);
+	}
+
+
+	public void setDay(DateTime today){
+		_day.setDay(today);
+		_clickableDay.setDay(today);
+		_today = today;
+		_label.setText(_today.dayOfWeek().getAsText() + " " + _today.monthOfYear().getAsShortText() + " " + _today.getDayOfMonth());
+		this.repaint();
+	}
+
+	public void paint(Graphics g){
+		super.paint(g);
+		this.revalidate();
+	}
+}
