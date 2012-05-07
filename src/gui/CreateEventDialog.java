44f33efaf6e868a8d9589bd074ffa2f8b56d79a0
Kelly Buckley <kellybuckley@Kelly-Buckleys-MacBook-Pro.local>
2012-05-07 04:10:51 -0400

    replaced Java logo with Kairos logo

--- src/gui/CreateEventDialog.java
+++ src/gui/CreateEventDialog.java
@@ -11,6 +11,7 @@ import java.awt.event.FocusListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
@@ -37,8 +38,9 @@ public class CreateEventDialog{
 	private DateTime _today;
 	private DateTime _firstOfMonth;
 	private ArrayList<DateTime> _selectedDates = new ArrayList<DateTime>();
-	private JComboBox<String> _startHour;
-	private JComboBox<String> _endHour;
+	private JComboBox _startHour;
+	private JComboBox _endHour;
+	private ImageIcon _kairosIcon = new ImageIcon("KairosIcon.png");
 	public static enum DaysOfWeek {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
 
 	//	public enum daysO()
@@ -98,10 +100,10 @@ public class CreateEventDialog{
 			}
 		}
 
-		_startHour = new JComboBox<String>(strings);
+		_startHour = new JComboBox(strings);
 		_startHour.setSelectedIndex(10);
 
-		_endHour = new JComboBox<String>(strings);
+		_endHour = new JComboBox(strings);
 		_endHour.setSelectedIndex(20);
 
 		JPanel times = new JPanel();
@@ -279,11 +281,11 @@ public class CreateEventDialog{
 		public void actionPerformed(ActionEvent e) {
 			if (e.getActionCommand().equals("OK")){
 				if (_eventName.getText().equals("New Event Name")){
-					JOptionPane.showMessageDialog(new JFrame(), "Please choose a name for your event.");
+					JOptionPane.showMessageDialog(new JFrame(), "Please choose a name for your event.", "", JOptionPane.ERROR_MESSAGE, _kairosIcon);
 				} else if (_selectedDates.size() == 0){
-					JOptionPane.showMessageDialog(new JFrame(), "Please choose dates for your event.");
+					JOptionPane.showMessageDialog(new JFrame(), "Please choose dates for your event.", "", JOptionPane.ERROR_MESSAGE, _kairosIcon);
 				} else if (_startHour.getSelectedIndex() >= _endHour.getSelectedIndex()){
-					JOptionPane.showMessageDialog(new JFrame(), "Please choose appropriate times for your event.");
+					JOptionPane.showMessageDialog(new JFrame(), "Please choose appropriate times for your event.", "", JOptionPane.ERROR_MESSAGE, _kairosIcon);
 				} else {
 					_dialog.dispose();
 					Collections.sort(_selectedDates);
@@ -295,7 +297,6 @@ public class CreateEventDialog{
 				_dialog.dispose();
 			}
 		}
-
 	}
 
 
