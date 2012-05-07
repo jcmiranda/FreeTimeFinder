6c5363663979aaac63b019d92b48745c1f34d08b
Jeanette Miranda <jeanette_miranda@brown.edu>
2012-05-07 10:33:27 -0400

    When2meet tests

--- src/calendar_importers_test/When2MeetTest.java
+++ src/calendar_importers_test/When2MeetTest.java
@@ -2,22 +2,31 @@ package calendar_importers_test;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.joda.time.DateTime;
+
 import calendar.CalendarSlots;
 import calendar.Event.CalByThatNameNotFoundException;
 import calendar.When2MeetEvent;
 import calendar_exporters.When2MeetExporter.NameAlreadyExistsException;
 import calendar_importers.When2MeetImporter;
 
+// Testing for importing of when2meets
+// Checks for start time, end time (corners to give start date and start time of day
+// together)
+// Respondees have correct names  and user ids
+// Event has correct ID and name
+
 public class When2MeetTest {
 	
-	private static void testImport(When2MeetImporter wtmi, String url, String name, HashMap<String, Integer> expectedRespondees, int eventID) throws IOException {
+	private static void testImport(When2MeetImporter wtmi, String url, DateTime expStartTime, DateTime expEndTime,
+			String expName, HashMap<String, Integer> expectedRespondees, int expEventID) throws IOException {
 			
 		When2MeetEvent event = wtmi.importNewEvent(url);
 		System.out.println("TESTING " + event.getName());
 		// Check the name of the event
-		assert(event.getName().equals(name));
+		assert(event.getName().equals(expName));
 		// Check the ID of the event
-		assert(event.getID() == eventID);
+		assert(event.getID() == expEventID);
 
 		// Build a list of all of the imported names
 		HashMap<String, Integer> importedNames = new HashMap<String, Integer>();
@@ -43,14 +52,22 @@ public class When2MeetTest {
 		HashMap<String, Integer> expRespondees = new HashMap<String, Integer>();
 		expRespondees.put("User1", 1872671);
 		expRespondees.put("User2", 1872676);
-		testImport(wtmi, "http://www.when2meet.com/?438771-KGv1R", "Test1", expRespondees, 438771);
+		DateTime startTime = new DateTime(2012, 5, 19, 9, 0);
+		DateTime endTime = new DateTime(2012, 5, 21, 5, 0);
+		testImport(wtmi, "http://www.when2meet.com/?438771-KGv1R", startTime,
+				endTime, "Test1", expRespondees, 438771);
 		
 		
-		// Test 2 - multi-word name, users with spaces in their names, to midnight
+		// NewTest Event Name - multi-word name, users with spaces in their names, to midnight
 		expRespondees.clear();
 		expRespondees.put("User 1", 1872681);
 		expRespondees.put("User 2", 1872686);
-		testImport(wtmi, "http://www.when2meet.com/?438776-5kHqQ", "NewTest Event Name", expRespondees, 438776);
+		startTime = new DateTime(2012, 5, 6, 9, 0);
+		endTime = new DateTime(2012, 5, 9, 11, 59);
+		testImport(wtmi, "http://www.when2meet.com/?438776-5kHqQ", startTime, endTime,
+				"NewTest Event Name", expRespondees, 438776);
+		
+		// 
 		
 	}
 }
