92169d4bf6c2c5921c7caa64298e4a9a6a0e05a0
Liz Neu <lizneu08@gmail.com>
2012-05-07 00:04:02 -0400

    Commenting cal_master classes

--- src/cal_master/ProgramOwner.java
+++ src/cal_master/ProgramOwner.java
@@ -2,6 +2,12 @@ package cal_master;
 
 import calendar.Owner;
 
+/**
+ * Represents all the information a user might want to store about themselves
+ * For now, that information is limited to the name they use to respond to events
+ *
+ */
+
 public class ProgramOwner implements Owner {
 
 	private String _name = null;
--- src/gui/CalendarGui.java
+++ src/gui/CalendarGui.java
@@ -12,8 +12,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
-import java.awt.Image;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
